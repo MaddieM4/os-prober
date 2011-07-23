MaddieM4/os-prober

commit 2ad9c80b0082917f0220abcf8798810614c702f8

item_in_dir: return 1 immediately if second argument is not a directory (thanks, Daniel Richard G.; LP: #798447).

common.sh
@@ -92,6 +92,7 @@ if ! type mapdevfs >/dev/null 2>&1; then
 fi
 
 item_in_dir () {
+	[ -d "$2" ] || return 1
 	if [ "$1" = "-q" ]; then
 		q="-q"
 		shift 1

commit b4118bc7d0ef7144cf1872833688a99f52ac823d

item_in_dir: fix regression regarding -q support
The commit 2ad9c80b0082917f0220abcf8798810614c702f8 has introduced a
regression when used with 3 params as:

item_in_dir -q foo dir/

So we need to shift params in case $1 to properly support it.

common.sh
@@ -92,13 +92,13 @@ if ! type mapdevfs >/dev/null 2>&1; then
 fi
 
 item_in_dir () {
-	[ -d "$2" ] || return 1
 	if [ "$1" = "-q" ]; then
 		q="-q"
 		shift 1
 	else
 		q=""
 	fi
+	[ -d "$2" ] || return 1
 	# find files with any case
 	ls -1 "$2" | grep $q -i "^$1$"
 }