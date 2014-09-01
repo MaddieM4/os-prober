f860ae3a9276f7792db578369e4bef1ad9da1f85
Philip Horger <campadrenalin@gmail.com>
2014-08-31 22:03:02 -0700

    Require unshare() to continue, add documentation

--- src/newns.c
+++ src/newns.c
@@ -12,15 +12,12 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	/* This is best-effort; if the kernel is too old (Linux << 2.6.16),
-	 * or indeed if the kernel isn't Linux so we don't have
-	 * unshare(CLONE_NEWNS), don't worry about it.
-	 */
-#ifdef __linux__
-	if (unshare(CLONE_NEWNS) < 0 && errno != ENOSYS)
-		perror("unshare failed");
-		/* ... but continue anyway */
-#endif /* __linux__ */
+	// We are not targeting any systems old enough not to support
+	// unshare(CLONE_NEWNS). Require it to continue.
+	if (unshare(CLONE_NEWNS) < 0 && errno != ENOSYS) {
+		perror("unshare failed (needed to prevent side effects, see man unshare)");
+		_exit(1);
+	}
 	setenv("OS_PROBER_NEWNS", "1", 1);
 	execvp(argv[1], argv + 1);
 
