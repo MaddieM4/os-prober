54943980bddf9c2440b44d3bceec210a8b125fe6
Philip Horger <campadrenalin@gmail.com>
2014-09-09 23:39:56 -0700

    Retab to 4-spaces

--- src/newns.c
+++ src/newns.c
@@ -7,20 +7,20 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 2) {
-		fprintf(stderr, "Usage: newns PROGRAM [ARGUMENTS ...]\n");
-		exit(1);
-	}
+    if (argc < 2) {
+        fprintf(stderr, "Usage: newns PROGRAM [ARGUMENTS ...]\n");
+        exit(1);
+    }
 
-	// We are not targeting any systems old enough not to support
-	// unshare(CLONE_NEWNS). Require it to continue.
-	if (unshare(CLONE_NEWNS) < 0 && errno != ENOSYS) {
-		perror("unshare failed (needed to prevent side effects, see man unshare)");
-		_exit(1);
-	}
-	setenv("OS_PROBER_NEWNS", "1", 1);
-	execvp(argv[1], argv + 1);
+    // We are not targeting any systems old enough not to support
+    // unshare(CLONE_NEWNS). Require it to continue.
+    if (unshare(CLONE_NEWNS) < 0 && errno != ENOSYS) {
+        perror("unshare failed (needed to prevent side effects, see man unshare)");
+        _exit(1);
+    }
+    setenv("OS_PROBER_NEWNS", "1", 1);
+    execvp(argv[1], argv + 1);
 
-	perror("execvp failed");
-	_exit(127);
+    perror("execvp failed");
+    _exit(127);
 }
